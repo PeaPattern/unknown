4d0202abbb431a5c21fdda3e84e65e84022331ec
PeaPattern <72760536+PeaPattern@users.noreply.github.com>
2024-02-09 01:09:44 -0500

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,2 +1,9 @@
 -- Made by PeaPattern
+
+_G.Config = {
+    Seperator = " ",
+    CommandBarHotkey = Enum.KeyCode.RightShift,
+    Killcam = true
+}
+    
 loadstring(game:HttpGet("https://raw.githubusercontent.com/PeaPattern/unknown/main/main.lua"))()
